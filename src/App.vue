f2817209c4b92d2be3905bb1e2c9205f283a4898
Gonzalo Barrazueta <u20191a972@upc.edu.pe>
2022-09-26 15:26:40 -0500

    feat: created Change Password component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import ChangePassword from "./components/ChangePassword.vue";
 </script>
 
 <template>
-    
+    <change-password></change-password>
 </template>
 
 <style scoped>
