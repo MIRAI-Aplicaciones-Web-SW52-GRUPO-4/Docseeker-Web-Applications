58bbfbb0e93a27dcc7692f2aceedd8b3cf73a770
Ken <kendallramiro@gmail.com>
2022-09-26 15:53:39 -0500

    feat(component): added Register component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import Register from "./components/Register.vue"
+
 </script>
 
 <template>
-    
+  <Register/>
+
 </template>
 
 <style scoped>
