a85de55403370eb22e6cf2eef20dedf84880a6ce
Gonzalo Barrazueta <u20191a972@upc.edu.pe>
2022-09-26 15:31:19 -0500

    feat: added HTML for the Change Password component using primevue

--- src/components/ChangePassword.vue
+++ src/components/ChangePassword.vue
@@ -1,5 +1,35 @@
 <template>
+  <h1>Change Of Password</h1>
 
+  <div id="changePasswordForm">
+    <div id="enterEmail">
+      <h3>Enter your email: </h3>
+      <div class="p-inputgroup" id="enterEmailInputContainer">
+        <span id="inputTextIcon" class="p-inputgroup-addon inputTextBorder">
+          <i class="pi pi-user"></i>
+        </span>
+        <pv-inputText id="email" class="inputTextBorder" placeholder="Email"></pv-inputText>
+      </div>
+    </div>
+
+    <div id="enterPassword">
+      <h3>Enter your password: </h3>
+      <div class="p-inputgroup" id="enterPasswordInputContainer">
+        <pv-inputText id="password" class="inputTextBorder" placeholder="Password"></pv-inputText>
+      </div>
+    </div>
+
+    <div id="repeatPassword">
+      <h3>Repeat your password: </h3>
+      <div class="p-inputgroup" id="repeatPasswordInputContainer">
+        <pv-inputText id="password" class="inputTextBorder" placeholder="Password"></pv-inputText>
+      </div>
+    </div>
+
+    <div id="submitBtnContainer">
+      <pv-button label="Submit" id="submitBtn" class="p-button-lg"/>
+    </div>
+  </div>
 </template>
 
 <script>
