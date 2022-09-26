11381945afc60de2b213f3d3ed8961b9903a97c8
Gonzalo Barrazueta <u20191a972@upc.edu.pe>
2022-09-26 15:34:26 -0500

    feat: added styling to the Change Password Component

--- src/components/ChangePassword.vue
+++ src/components/ChangePassword.vue
@@ -15,14 +15,14 @@
     <div id="enterPassword">
       <h3>Enter your password: </h3>
       <div class="p-inputgroup" id="enterPasswordInputContainer">
-        <pv-inputText id="password" class="inputTextBorder" placeholder="Password"></pv-inputText>
+        <pv-inputText id="password" type="password" class="inputTextBorder" placeholder="Password"></pv-inputText>
       </div>
     </div>
 
     <div id="repeatPassword">
       <h3>Repeat your password: </h3>
       <div class="p-inputgroup" id="repeatPasswordInputContainer">
-        <pv-inputText id="password" class="inputTextBorder" placeholder="Password"></pv-inputText>
+        <pv-inputText type="password" id="password" class="inputTextBorder" placeholder="Password"></pv-inputText>
       </div>
     </div>
 
@@ -40,4 +40,67 @@ export default {
 
 <style scoped>
 
+#changePasswordForm {
+  /*border: 5px solid crimson;*/
+  display: flex;
+  flex-flow: column;
+  justify-content: space-between;
+}
+
+#enterEmail {
+  /*border: 5px solid #2980B9;*/
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  margin: 2% 0;
+}
+
+#enterEmailInputContainer {
+  /*border: 5px solid #ffed3d;*/
+  width: 50%;
+}
+
+#enterPassword {
+  /*border: 5px solid #2980B9;*/
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  margin: 2% 0;
+}
+
+#enterPasswordInputContainer {
+  /*border: 5px solid #ffed3d;*/
+  width: 50%;
+}
+
+#repeatPassword {
+  /*border: 5px solid #2980B9;*/
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  margin: 2% 0;
+}
+
+#repeatPasswordInputContainer {
+  /*border: 5px solid #ffed3d;*/
+  width: 50%;
+}
+
+#submitBtnContainer {
+  /*border: 5px solid #ffed3d;*/
+  display: flex;
+  justify-content: center;
+}
+
+#submitBtn {
+  background-color: #0093AB;
+}
+
+.inputTextBorder {
+  border: 3px solid #2980B9;
+}
+
+#inputTextIcon {
+  border-right: none;
+}
 </style>
