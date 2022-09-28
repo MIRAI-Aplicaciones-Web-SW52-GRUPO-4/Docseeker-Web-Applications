b09e1b1924118b4f3021fca94bfe1c877b41f468
Ken <kendallramiro@gmail.com>
2022-09-28 16:06:56 -0500

    feat(fix): deleted imports unused

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import RegisterView from './views/RegisterView.vue'
-import ChangePassword from "./components/ChangePassword.vue";
 import SearchDoctorView from "./views/SearchDoctorView.vue"
 </script>
 
