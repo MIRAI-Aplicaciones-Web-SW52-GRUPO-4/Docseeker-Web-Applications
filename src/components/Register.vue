f8e5569433fb43db1b0cfb598e86d63e89f9ce02
Matthew Tello <U202021443@upc.edu.pe>
2022-10-21 23:58:49 -0500

    feat(Register): added json validation for register view.

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -6,21 +6,21 @@
       <div class="inputContainers">
         <div class="inputContainer">
           <label for="dni">DNI Number / CE</label>
-          <pv-inputText id="dni" type="number"/>
+          <pv-inputText v-model="dni" id="dni" type="number"/>
         </div>
         <div class="inputContainer">
           <label for="name">Full Name</label>
-          <pv-inputText id="name" type="text" />
+          <pv-inputText v-model="name" id="name" type="text" />
         </div>
       </div>
       <div class="inputContainers">
         <div class="inputContainer">
           <label for="paternal-name">Paternal Name</label>
-          <pv-inputText id="paternal-name" type="text" />
+          <pv-inputText v-model="paternalName" id="paternal-name" type="text" />
         </div>
         <div class="inputContainer">
           <label for="maternal-name">Maternal Name</label>
-          <pv-inputText id="maternal-name" type="text" />
+          <pv-inputText v-model="maternalName" id="maternal-name" type="text" />
         </div>
       </div>
       <div class="inputContainers">
@@ -39,16 +39,16 @@
       <h3>Contact</h3>
       <div class="inputContainer fullSize">
         <label for="email">Email</label>
-        <pv-inputText id="email" type="email"/>
+        <pv-inputText v-model="email" id="email" type="email"/>
       </div>
       <div class="inputContainers">
         <div class="inputContainer">
           <label for="cellphone1">Cellphone 1</label>
-          <pv-inputText id="cellphone1" type="number" />
+          <pv-inputText v-model="cellphone1" id="cellphone1" type="number" />
         </div>
         <div class="inputContainer">
           <label for="cellphone2">Cellphone 2</label>
-          <pv-inputText id="cellphone2" type="number" />
+          <pv-inputText v-model="cellphone2" id="cellphone2" type="number" />
         </div>
       </div>
     </div>
@@ -58,7 +58,7 @@
       <div class="inputContainers">
         <div class="inputContainer">
           <label for="password1">Password</label>
-          <pv-inputText id="password1" type="password" placeholder="Password"/>
+          <pv-inputText v-model="password" id="password1" type="password" placeholder="Password"/>
         </div>
         <div class="inputContainer">
           <label for="password2">Repeat Password</label>
@@ -67,13 +67,15 @@
       </div>
     </div>
     <div class="buttonContainer">
-      <router-link to="/logIn"> <pv-button label="Register" class="p-button-lg"></pv-button> </router-link>
+      <router-link to="/logIn"> <pv-button @click="addPatient()" label="Register" class="p-button-lg"></pv-button> </router-link>
     </div>
 
 
   </main>
 </template>
 <script>
+
+import {PatientsApiService} from "../learning/services/patients-api.service";
 
 export default {
 
@@ -84,7 +86,43 @@ export default {
         {name: 'Male', code: 'M'},
         {name: 'Female', code: 'F'}
       ],
-      value: null
+      value: null,
+      dni: null,
+      name: null,
+      paternalName: null,
+      maternalName: null,
+      birthday: null,
+      email: null,
+      cellphone1: null,
+      cellphone2: null,
+      password: null,
+      patients: [],
+      patient: {},
+      patientsService: null
+    }
+  },
+  created() {
+    this.patientsService = new PatientsApiService();
+    this.patientsService.getAll().then((response) => {
+      this.patients = response.data;
+    });
+  }, methods: {
+    addPatient(){
+      this.patient = {
+        id: this.patients.length,
+        DNI: this.dni,
+        name: this.name + " " + this.paternalName + " " + this.maternalName,
+        genre: this.selectedGenre,
+        birthday: this.value,
+        email: this.email,
+        cell1: this.cellphone1,
+        cell2: this.cellphone2,
+        password: this.password,
+        photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/Emblem-person-blue.svg/2048px-Emblem-person-blue.svg.png"
+      }
+
+      this.patientsService.create(JSON.stringify(this.patient));
+
     }
   }
 
