d3e5c849ba82ad8ba51b25a8dc64ae1db9efc749
Matthew Tello <U202021443@upc.edu.pe>
2022-10-21 23:58:29 -0500

    feat(New): added json validation for new view.

--- src/views/patients/NewView.vue
+++ src/views/patients/NewView.vue
@@ -1,59 +1,74 @@
 <template>
-    <div class="backButton">
-        <MenuBar></MenuBar>
-        <router-link to="/news"><i class="pi pi-angle-left" style="font-size: 200%"></i></router-link>
-        <h1>News</h1>
-    </div>
-    <div class="newContainer">
-        <h1 class="newTitle">{{_new.title}}</h1>
-        <img class="newImg" :src="_new.image" alt="image new">
-        <p class="newText">{{_new.info}}</p>
-    </div>
+  <div class="backButton">
+    <MenuBar></MenuBar>
+    <router-link to="/news"><i class="pi pi-angle-left" style="font-size: 200%"></i></router-link>
+    <h1><strong>News</strong></h1>
+  </div>
+  <div class="newContainer">
+    <h1 class="newTitle">{{this.new.title}}</h1>
+    <img class="newImg" :src="this.new.image" alt="image new">
+    <p class="newText">{{this.new.info}}</p>
+  </div>
 </template>
 <script setup>
-    import MenuBar from "../../components/MenuBar.vue"
+import MenuBar from "../../components/MenuBar.vue"
 </script>
 <script>
-    import { useCounterStore } from "../../stores/counter";
-    export default {
-    data(){
-        return{
-            imgUrl: null,
-            _new: useCounterStore().news
+import {NewsApiService} from "../../learning/services/news-api.services";
+
+export default {
+  name: "New",
+  data() {
+    return {
+      news: [],
+      newsService: null,
+      new: {},
+    };
+  },
+  created(){
+    this.newsService = new NewsApiService();
+    this.newsService.getAll().then((response) => {
+      this.news = response.data;
+      this.news.forEach((newss) =>
+          this.getDisplayableTutorial(newss)
+      );
+
+      for (let x in this.news){
+        if (this.news[x].id == this.$route.params.id){
+          this.new = this.news[x];
+          break;
         }
-    },
-    created() {
-        for (let x in this._new){
-            if (this._new[x].id == this.$route.params.id){
-                this._new = this._new[x];
-                useCounterStore().newViewed(x);
-                console.log(this._new, "ya esta")
-                break;
-            }
-        }
-        this.imgUrl = `../../src/assets/images/${this._new.name} image.jpg`
+      }
+
+    });
+  },
+  methods: {
+    getDisplayableTutorial(newss) {
+      return newss;
     }
+  }
 }
+
 </script>
 <style scoped>
-    .backButton{
-      display:flex;
-      align-items: center;
-    }
-    .newContainer{
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        padding: 4rem;
-        background-color: #F0F0F0;
-        border-radius: 15px;
-        border: 1px solid #0093AB;
-        width: 70%;
-        margin: 0 auto;
-        gap: 2rem;
-    }
-    img{
-        border-radius: 15px;
-    }
+.backButton{
+  display:flex;
+  align-items: center;
+}
+.newContainer{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 4rem;
+  background-color: #F0F0F0;
+  border-radius: 15px;
+  border: 1px solid #0093AB;
+  width: 70%;
+  margin: 0 auto;
+  gap: 2rem;
+}
+img{
+  border-radius: 15px;
+}
 </style>
