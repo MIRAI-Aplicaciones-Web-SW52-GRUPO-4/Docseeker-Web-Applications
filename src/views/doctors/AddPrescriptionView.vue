e9421b903b2e56c9e171ba5d9118efe00b549946
Luis Pineda <u20201e705@upc.edu.pe>
2022-10-27 17:39:23 -0500

    feat(prescription): added view for prescription

---
+++ src/views/doctors/AddPrescriptionView.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="backButton">
+        <MenuBarDoctor></MenuBarDoctor>
+        <router-link to="/dashboardDoctor"><i class="pi pi-angle-left" style="font-size: 200%"></i></router-link>
+        <h1>Your Patients</h1>
+    </div>
+    <h3>Here, you can see all your patients to make them a new prescription</h3>
+    <PatientPrescription v-for="patient in patientsOfDoctor" :patient="patient" ></PatientPrescription>
+
+</template>
+<script setup>
+    import MenuBarDoctor from "../../components/MenuBarDoctor.vue"
+    import PatientPrescription from "../../components/PatientPrescription.vue"
+</script>
+<script>
+ import {useCounterStore} from "../../stores/counter"
+    export default {
+    data(){
+        return{
+            userId: 0, /* ESTO DEBE SER LA ID DEL USUARIO QUE ESTÉ LOGEADO */
+            dates: useCounterStore().dates,
+            patients: useCounterStore().patients,
+            patientsId: [],
+            patientsOfDoctor: []
+        }
+    },
+    created() {
+
+    /* OBTENER LISTA DE PACIENTES QUE REALIZARON ALGUNA VEZ UNA CITA CON EL DOCTOR LOGEADO */
+
+        for (let x in this.dates){
+            if ((this.dates[x].doctorId == this.userId) && (this.patientsId.indexOf(this.dates[x].idPatient) === -1)){
+                this.patientsId.push(this.dates[x].idPatient)
+            }
+        }
+        for (let x in this.patients){
+            for (let y in this.patientsId){
+                if (this.patients[x].id == this.patientsId[y]){
+                    this.patientsOfDoctor.push(this.patients[x])
+                }
+            }
+        }
+    }
+}
+</script>
+<style scoped>
+    .backButton{
+      display:flex;
+      align-items: center;
+    }
+</style>
