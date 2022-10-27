b4ec18fd1cec6da498f5d85e79b4a011fe7c11d5
Ken <kendallramiro@gmail.com>
2022-10-27 17:43:45 -0500

    feat(created): created LogInView for doctor

---
+++ src/views/doctors/LogInView.vue
@@ -0,0 +1,33 @@
+<script setup>
+    import LogInCardDoctor from '../../components/LogInCardDoctor.vue'
+  
+  </script>
+  <template>
+    <div class="container">
+        <pv-card-content class="welcomeImgCard ">
+            <img src="../../assets/images/doctor.png"
+            alt="doctor login" class="images-Welcome">
+            <h1>Doctor</h1>
+        </pv-card-content>
+        <LogInCardDoctor/>
+    </div>
+  </template>
+  
+  
+  <style scoped>
+    .container{
+        display: flex;
+        justify-content: space-evenly;
+        align-items: center;
+    }
+    .welcomeImgCard{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+
+    }
+    .images-Welcome{
+        width: 700px;
+    }
+  </style>
