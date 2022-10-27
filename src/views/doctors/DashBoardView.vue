b659dbcf4b844462ae41ca698c332a2e5864989f
Ken <kendallramiro@gmail.com>
2022-10-27 17:44:14 -0500

    feat(created): created Dashboard using bottons and menubar from doctors

---
+++ src/views/doctors/DashBoardView.vue
@@ -0,0 +1,73 @@
+<template >
+    <div>
+        <div class="containerTitle">
+            <MenuBarDoctor></MenuBarDoctor>
+            <h1 class="title">Welcome User</h1>
+        </div>
+        <h2 class="newsTitle">¡Recent News!</h2>
+        <pv-carousel :value="news" :numVisible="2" :numScroll="1" :circular="true" :autoplayInterval="3000" style="max-width: 100vw;">
+            <template #item="slotProps">
+                <h1>{{slotProps.data.title}}</h1>
+                <p>{{slotProps.data.description}}</p>
+                <img :src="slotProps.data.image"/>
+            </template>
+        </pv-carousel>
+        <h2 class="buttonsTitle">¿What do you need?</h2>
+        <CardsButtonDoctor></CardsButtonDoctor>
+    </div>
+</template>
+<script setup>
+import CardsButtonDoctor from '../../components/CardsButtonDoctor.vue';
+import MenuBarDoctor from '../../components/MenuBarDoctor.vue';
+
+const news = [
+            {
+                title: "noticia 1",
+                description: "no ce manito no importa",
+                image: "https://picsum.photos/id/230/600/300"
+            },
+            {
+                title: "noticia 2",
+                description: "facilito",
+                image: "https://picsum.photos/id/231/600/300"
+            },
+            {
+                title: "noticia 3",
+                description: "jhdksfjhkdfs",
+                image: "https://picsum.photos/id/232/600/300"
+            },
+            {
+                title: "noticia 4",
+                description: "w235342",
+                image: "https://picsum.photos/id/233/600/300"
+            },
+            {
+                title: "noticia 5",
+                description: "no23466o",
+                image: "https://picsum.photos/id/234/600/300"
+            },
+            {
+                title: "ya termino, devuelta al start",
+                description: "3456fghfghf",
+                image: "https://picsum.photos/id/235/600/300"
+            }
+        ]
+</script>
+
+<style scoped>
+    .title{
+        font-weight: bolder;
+    }
+    .newsTitle{
+        text-align: center;
+        color: gray;
+        margin: 1rem;
+    }
+    .containerTitle{
+        display: flex;
+    }
+    .buttonsTitle{
+        font-weight: bolder;
+        margin: 2rem;
+    }
+</style>
