cd5c29b1077fb51294cbfbbd10c0b435a4596590
Matthew Tello <U202021443@upc.edu.pe>
2022-10-28 16:33:17 -0500

    feat(newPrescription): added view for new prescription.

---
+++ src/views/doctors/NewPrescriptionView.vue
@@ -0,0 +1,211 @@
+<template>
+  <div class="backButton">
+    <MenuBarDoctor></MenuBarDoctor>
+    <router-link to="/addPrescription"><i class="pi pi-angle-left" style="font-size: 200%"></i></router-link>
+    <h1>New prescription for {{patient.name}}</h1>
+  </div>
+  <h3>Register a new prescription for {{patient.name}}.</h3>
+  <div class="prescription">
+    <div class="containerInfo">
+      <img :src="patient.photo" alt="patient image">
+      <div class="info">
+        <h3>Name: {{patient.name}}</h3>
+        <h3>Genre: {{patient.genre}}</h3>
+      </div>
+    </div>
+    <div class="inputs">
+      <div class="input">
+        <label for="calendarr">Date Issue</label>
+        <pv-calendar v-model="dateIssue" dateFormat="dd.mm.yy" id="calendarr" placeholder="Day/Month/Year" />
+      </div>
+      <div class="input">
+        <label for="calendarr">Date Expiration</label>
+        <pv-calendar v-model="dateExpiration" dateFormat="dd.mm.yy" id="calendarr" placeholder="Day/Month/Year" />
+      </div>
+      <div class="input">
+        <label for="calendarr">Recip Code</label>
+        <pv-inputNumber v-model="recipCode"></pv-inputNumber>
+      </div>
+      <div class="input textArea">
+        <label for="calendarr">Rest routine</label>
+        <pv-textArea v-model="restRoutine" class="textArea"/>
+      </div>
+      <div class="input textArea">
+        <label for="calendarr">Drink routine</label>
+        <pv-textArea v-model="drinkRoutine" class="textArea"/>
+      </div>
+      <div class="input textArea">
+        <label for="calendarr">Meals routine</label>
+        <pv-textArea v-model="mealsRoutine" class="textArea"/>
+      </div>
+      <div class="input">
+        <label>Medicines (Separate with a ',')</label>
+        <pv-inputText class="inputField" v-model="medicines" />
+      </div>
+      <div class="input">
+        <label for="calendarr">Number of Dose</label>
+        <pv-inputNumber v-model="numberDose"></pv-inputNumber>
+      </div>
+      <div class="input">
+        <label>Meals indicator (Separate with a ',')</label>
+        <pv-inputText class="inputField" v-model="mealsIndicator" />
+      </div>
+      <div class="input">
+        <label>Hours of Dose (Separate with a ',')</label>
+        <pv-inputText class="inputField" v-model="hoursDose" />
+      </div>
+    </div>
+    <pv-button class="btn" label="Save" @click="newPrescription"></pv-button>
+
+  </div>
+</template>
+<script setup>
+import MenuBarDoctor from "../../components/MenuBarDoctor.vue"
+import { ref } from "vue"
+let dateIssue = ref("")
+let dateExpiration = ref("")
+let recipCode = ref("")
+let restRoutine = ref("")
+let drinkRoutine = ref("")
+let mealsRoutine = ref("")
+let medicines = ref("")
+let numberDose = ref("")
+let mealsIndicator = ref("")
+let hoursDose = ref("")
+</script>
+<script>
+import { useCounterStore } from "../../stores/counter";
+
+export default {
+  data(){
+    return{
+      patient: {},
+      doctor: {},
+      userId: 0 /* ESTE DEBE SER EL ID DEL USUARIO LOGEADO */
+    }
+  },
+  created() {
+    for (let x in useCounterStore().patients){
+      if (useCounterStore().patients[x].id == this.$route.params.id){
+        this.patient = useCounterStore().patients[x];
+        break;
+      }
+    }
+    for (let x in useCounterStore().doctors){
+      if (useCounterStore().doctors[x].id == this.userId){
+        this.doctor = useCounterStore().doctors[x];
+        break;
+      }
+    }
+  },
+  methods: {
+    separatingArrays(){
+      this.medicines=this.medicines.split(",")
+      this.mealsIndicator=this.mealsIndicator.split(",")
+      this.hoursDose=this.hoursDose.split(",")
+      let arraymedicines = []
+      this.medicines.forEach((element, index) => {
+        arraymedicines.push(
+            {
+              id: index,
+              medicine: element
+            }
+        )
+      });
+      this.medicines = arraymedicines
+
+      if(this.medicines.length==1 && this.medicines[0].medicine===""){
+        this.medicines=[]
+      }
+      if(this.mealsIndicator.length==1 && this.mealsIndicator[0]===""){
+        this.mealsIndicator=[]
+      }
+      if(this.hoursDose.length==1 && this.hoursDose[0]===""){
+        this.hoursDose=[]
+      }
+    },
+    newPrescription(){
+      this.separatingArrays()
+      useCounterStore().prescriptions.push(
+          {
+            id: useCounterStore().prescriptions.length,
+            idPatient: this.patient.id,
+            dateIssue: this.dateIssue.getDate() + "/" + (this.dateIssue.getMonth()+1) + "/" + this.dateIssue.getFullYear(),
+            dateExpiration: this.dateExpiration.getDate() + "/" + (this.dateExpiration.getMonth()+1) + "/" + this.dateExpiration.getFullYear(),
+            medicalSpeciality: this.doctor.area,
+            recipCode: this.recipCode,
+            condition: "Valid",
+            rest: ` ${this.restRoutine}`,
+            drink: ` ${this.drinkRoutine}`,
+            food: ` ${this.mealsRoutine}`,
+            medicines: this.medicines,
+            numberDose: this.numberDose,
+            meals: this.mealsIndicator,
+            hours: this.hoursDose
+          }
+      )
+      this.$router.push({name: 'addPrescription'})
+      console.log(useCounterStore().prescriptions)
+    }
+  }
+}
+</script>
+<style scoped>
+.backButton{
+  display:flex;
+  align-items: center;
+}
+.prescription{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 70%;
+  margin: 1rem auto;
+  padding: 2rem;
+  background-color: #CBDDE0;
+  border: 1px solid #3CC1D7;
+  border-radius: 15px;
+}
+.containerInfo{
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 2rem;
+  height: 200px;
+  margin-bottom: 1rem;
+}
+.info{
+  display: flex;
+  flex-direction: column;
+}
+.info h3{
+  font-weight: bolder;
+}
+.inputs{
+  width: 70%;
+  margin: 0 auto;
+  padding: 2rem;
+  background-color: #CBDDE0;
+  border: 1px solid #3CC1D7;
+  border-radius: 15px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 1rem;
+  /* flex-direction: column;
+  justify-content: center; */
+}
+.input{
+  display: flex;
+  flex-direction: column;
+}
+.input label{
+  font-weight: bolder;
+}
+
+.btn{
+  margin: 1rem;
+  background-color: #2980B9;
+  width: 50%;
+}
+</style>
