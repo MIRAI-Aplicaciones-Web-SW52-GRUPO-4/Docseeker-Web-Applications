59cc9c25d965dbfda6f5cdcf391f0c972882d14e
Ken <kendallramiro@gmail.com>
2022-10-29 18:00:46 -0500

    feat(update): update DashboardViewDoctor with json files

--- src/views/doctors/DashBoardView.vue
+++ src/views/doctors/DashBoardView.vue
@@ -5,11 +5,12 @@
             <h1 class="title">Welcome User</h1>
         </div>
         <h2 class="newsTitle">¡Recent News!</h2>
-        <pv-carousel :value="news" :numVisible="2" :numScroll="1" :circular="true" :autoplayInterval="3000" style="max-width: 100vw;">
+        <pv-carousel :value="news" :numVisible="2" :numScroll="1" :circular="true" :autoplayInterval="3000" :responsiveOptions="responsiveOptions" style="max-width: 100vw;">
             <template #item="slotProps">
-                <h1>{{slotProps.data.title}}</h1>
-                <p>{{slotProps.data.description}}</p>
-                <img :src="slotProps.data.image"/>
+                <h3>{{ slotProps.data.title }}</h3>
+                <div class="containerImg">
+                  <img :src="slotProps.data.image"/>
+                </div>
             </template>
         </pv-carousel>
         <h2 class="buttonsTitle">¿What do you need?</h2>
@@ -19,41 +20,52 @@
 <script setup>
 import CardsButtonDoctor from '../../components/CardsButtonDoctor.vue';
 import MenuBarDoctor from '../../components/MenuBarDoctor.vue';
-
-const news = [
-            {
-                title: "noticia 1",
-                description: "no ce manito no importa",
-                image: "https://picsum.photos/id/230/600/300"
-            },
-            {
-                title: "noticia 2",
-                description: "facilito",
-                image: "https://picsum.photos/id/231/600/300"
-            },
-            {
-                title: "noticia 3",
-                description: "jhdksfjhkdfs",
-                image: "https://picsum.photos/id/232/600/300"
-            },
-            {
-                title: "noticia 4",
-                description: "w235342",
-                image: "https://picsum.photos/id/233/600/300"
-            },
-            {
-                title: "noticia 5",
-                description: "no23466o",
-                image: "https://picsum.photos/id/234/600/300"
-            },
-            {
-                title: "ya termino, devuelta al start",
-                description: "3456fghfghf",
-                image: "https://picsum.photos/id/235/600/300"
-            }
-        ]
 </script>
 
+<script>
+import {NewsApiService} from "../../learning/services/news-api.service";
+
+export default{
+  data() {
+    return {
+      news: [],
+      newsService: null,
+      new: {},
+      responsiveOptions: [
+        {
+          breakpoint: '1024px',
+          numVisible: 3,
+          numScroll: 3
+        },
+        {
+          breakpoint: '600px',
+          numVisible: 2,
+          numScroll: 2
+        },
+        {
+          breakpoint: '480px',
+          numVisible: 1,
+          numScroll: 1
+        }
+      ]
+    }
+  },
+  created(){
+    this.newsService = new NewsApiService();
+    this.newsService.getAll().then((response) => {
+      this.news = response.data;
+      this.news.forEach((newss) =>
+          this.getDisplayableTutorial(newss)
+      );
+    });
+  },
+  methods: {
+    getDisplayableTutorial(newss) {
+      return newss;
+    }
+  }
+}
+</script>
 <style scoped>
     .title{
         font-weight: bolder;
@@ -70,4 +82,13 @@ const news = [
         font-weight: bolder;
         margin: 2rem;
     }
+    img{
+      width:300px;
+    }
+    .containerImg{
+      width:100%;
+      text-align: center;
+      margin:1rem;
+    }
+
 </style>
