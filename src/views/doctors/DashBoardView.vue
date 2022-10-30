dbdffc68f3ab34e7bfcd9d42ac8aa7e947a0e275
Ken <kendallramiro@gmail.com>
2022-10-29 19:48:33 -0500

    feat(fixed): fixed DashboardViewDoctor and data from json

--- src/views/doctors/DashBoardView.vue
+++ src/views/doctors/DashBoardView.vue
@@ -38,9 +38,9 @@ export default{
           numScroll: 3
         },
         {
-          breakpoint: '600px',
-          numVisible: 2,
-          numScroll: 2
+          breakpoint: '750px',
+          numVisible: 1,
+          numScroll: 1
         },
         {
           breakpoint: '480px',
