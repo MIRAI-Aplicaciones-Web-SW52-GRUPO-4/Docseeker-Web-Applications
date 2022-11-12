200565aa1dc574987d61bf0b7e6fc653699096ad
Edgar <u202020371@upc.edu.pe>
2022-11-12 10:39:42 -0500

    feat: added comment

--- src/core/services/http-common.js
+++ src/core/services/http-common.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://localhost:7263/api/v1', //returns the base url
+    baseURL: 'https://localhost:7263/api/v1', //returns the base url in the port of the backend solution
     headers: { 'Content-type': 'application/json' }
 });
